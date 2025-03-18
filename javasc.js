ab67cdd080d799a875c8a14d22c80177f0cfe421
N1ceTree <ifcc.calvin@gmail.com>
2025-03-19 05:28:33 +0800

    added everything

---
+++ javasc.js
@@ -0,0 +1,57 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+
+let websiteamt = document.getElementsByClassName("");
+
+for (j = 0; j < dots.length; j++) {
+
+}
+
+function toggleDropdown() {
+    var section = document.getElementById("dropdown-section");
+    section.classList.toggle("hidden");
+}
+
+
+
+
+
+
+
+
+
+
+
+
